7350e72ccd0661962e2376502085353c159676f8
rakshith-gannarapu <59986312+rakshith-gannarapu@users.noreply.github.com>
2020-07-12 16:55:03 -0400

    Create seed.groovy

---
+++ seed.groovy
@@ -0,0 +1,48 @@
+import groovy.json.JsonSlurper
+
+folder("Rakshith/patchset")
+
+imagesJson = '''{ "github":[{
+                                "repo":"https://github.com/rakshith-gannarapu/softeng.git",
+                                "directory":"Rakshith/patchset/promenade",
+                                "image":"multi-node-promenade",
+                                "name":"promenade",
+                                "jenkinsfile_loc":"Jenkinsfile"
+                            }]}'''
+
+jsonSlurper = new JsonSlurper()
+object = jsonSlurper.parseText(imagesJson)
+
+for (entry in object.github) {
+  folder("${entry.directory}")
+	git clone ${entry.repo}
+    pipelineJob("${entry.directory}/${entry.image}") {
+        logRotator{
+            daysToKeep(90)
+        }
+        parameters {
+            stringParam {
+                defaultValue("${entry.repo}")
+                description('sample pipelineJob')
+                name ('GIT_REPO')
+                trim(true)
+            }
+        }
+        configure {
+            node -> node / 'properties' / 'jenkins.branch.RateLimitBranchProperty_-JobPropertyImpl'{
+                durationName 'hour'
+                count '10'
+            }
+        }
+        triggers {
+            scmpoll_spec("* * * * *")
+
+           definition {
+               cps {
+                 script(readFileFromWorkspace("Rakshith/patchset/${entry.jenkinsfile}"))
+                   sandbox(false)
+               }
+           }
+        }
+    }
+}
